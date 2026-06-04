fdfead9f82f79e37f2617a16d2ce2bd40748fae4
Sima-akhter <simaakter5301@gmail.com>
2026-06-04 20:52:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://studyNook:Rm23YdaXaFcJBbkI@programming-hero.ifoutmp.mongodb.net/?appName=programming-hero`;
+
+
+// Rm23YdaXaFcJBbkI;
+// studyNook;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally {
+// await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Study Nook Server is running");
+})
+
+app.listen(port, () => {
+    console.log(`Study Nook Server is running on port ${port}`);
+})
