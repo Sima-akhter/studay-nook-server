62813d6ee45f775944985324db9c1153e18a6999
Sima-akhter <simaakter5301@gmail.com>
2026-06-06 20:08:48 +0600

    update bd url

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://studyNook:Rm23YdaXaFcJBbkI@programming-hero.ifoutmp.mongodb.net/?appName=programming-hero`;
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@programming-hero.ifoutmp.mongodb.net/?appName=programming-hero`;
 
 
-// Rm23YdaXaFcJBbkI;
-// studyNook;
+
 
 const client = new MongoClient(uri, {
   serverApi: {
