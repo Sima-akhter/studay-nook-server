86ba6fc1f83dad5368a03901a22ce7f81d083b8d
Sima-akhter <simaakter5301@gmail.com>
2026-06-14 14:04:42 +0600

    update env file

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@programming-hero.ifoutmp.mongodb.net/?appName=programming-hero`;
-
-
-
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,21 +19,22 @@ const client = new MongoClient(uri, {
 });
 
 const run = async () => {
-    try {
-        await client.connect();
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    }
-    finally {
-// await client.close();
-    }
-}
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } finally {
+    // await client.close();
+  }
+};
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-    res.send("Study Nook Server is running");
-})
+  res.send("Study Nook Server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Study Nook Server is running on port ${port}`);
-})
+  console.log(`Study Nook Server is running on port ${port}`);
+});
